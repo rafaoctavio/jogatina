b8a62032758b489e8cc93f1caefc0c6451aaf93b
rafaoctavio <rafaoctavio@gmail.com>
2020-08-23 21:00:20 -0300

    adicionando express no projeto

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const http = require ('http');
+const validador = require('')
 
 http.createServer((req, res)=> {
-    res.writeHead(200, {"Content-type": "text/plain"})
-    res.end("Meu primeiro servidor");
+    // res.writeHead(200, {"Content-type": "text/plain"})
+    // res.end("Meu primeiro servidor");
+
+    // switch(req.url) {
+    //     case "/login.html":
+    //     res.end("login.html");
+    //     break;
+    //     case "/cadastro.html":
+    //     res.end ("cadastro.html");
+    //     break;
+
+    // }
 
     switch(req.url) {
-        case "/login.html":
-        res.end("login.html");
-        break;
-        case "/cadastro.html":
-        res.end ("cadastro.html");
-        break;
+            case "/login.html":
+            res.end("login.html");
+            break;
+            case "/cadastro.html":
+            res.end ("cadastro.html");
+            break;
 
-    }
 
 }).listen(3000, "localhost");
 
