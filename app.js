f97d21d406a41bc39568f0a82e235fda31da33cf
rafaoctavio <rafaoctavio@gmail.com>
2020-08-31 12:44:51 -0300

    instalando template engine

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 
-app.get('/home', (req, res) => res.send('servidor rodando'));
+app.listen (3000,  () => {
+    console.log ("==========SERVIDOR ONLINE========");
+});
+
+
