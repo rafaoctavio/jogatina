ae8d2b599d6618ef4360e4b7c73fcb96a0c269a5
rafaoctavio <rafaoctavio@gmail.com>
2020-09-01 01:19:30 -0300

    servidor up and running

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', casdastroRouter);
+app.use('/', cadastroRouter);
 app.use('/amigos', amigosRouter);
 app.use('/profile', profileRouter);
 app.use('/login', loginRouter);
