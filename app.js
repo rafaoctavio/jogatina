7b079434395cb330650bdf0c4b438c20701617df
rafaoctavio <rafaoctavio@gmail.com>
2020-08-31 22:42:13 -0300

    iniciando o projeto

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.set('view engine', 'ejs');
-
-app.listen (3000,  () => {
-    console.log ("==========SERVIDOR ONLINE========");
-});
-
-
