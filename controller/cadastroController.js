e1c915c6ea1eff74236a8df095dfa5d161e79bb2
rafaoctavio <rafaoctavio@gmail.com>
2020-11-23 14:36:31 -0300

    criando conexão database-models-migrations-view-backend

--- controller/cadastroController.js
+++ controller/cadastroController.js
@@ -1,7 +1,11 @@
+const {usuario} = require ('../models')
 const CadastroController = {
-    index: (req, res) => {
-        return res.render("cadastro");
+    criar: async (req, res) => {
+        const dados = req.body;
+        const result = await Usuario.create(dados);
+        return res.redirect('/profile/');
     }
+ 
 }
 
 module.exports = CadastroController;
